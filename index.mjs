c6554283e91f5263d6b4ed219474a639a568a28f
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-07-24 19:08:35 +0900

    YouTube Data APIを使う

--- index.mjs
+++ index.mjs
@@ -1,5 +1,12 @@
-import {writeFileText, Youtube} from "hzkl"
-const url = "https://www.youtube.com/channel/UCSMOQeBJ2RAnuFungnQOxLg"
+import { writeFileText, writeFileJson, Youtube } from "hzkl";
+const url = "https://www.youtube.com/c/BlenderFoundation";
 
 const channelId = await Youtube.getChannelIdFromCustomUrl(url);
 writeFileText("./output/channelId.txt", channelId);
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const yt = new Youtube(process.env.YOUTUBE_API_KEY);
+const channels = await yt.getChannels([channelId])
+writeFileJson("./output/channels.json", channels);
